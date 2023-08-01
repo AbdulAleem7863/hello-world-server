261438384c8afc4562d6e449ca9979f4aed9aa8e
AbdulAleem7863 <abdulaleemnaqshbandi1234@gmail.comj>
2023-08-01 12:00:34 -0700

    hello world code

---
+++ server.mjs
@@ -0,0 +1,18 @@
+console.log("this is express server hello world");
+
+import express from 'express';
+const app = express()
+const PORT = 3000
+
+app.get('/', (req, res) => {
+    console.log('Hello World!',new Date());
+  res.send('Hello World!'+new Date())
+})
+app.get('/profile', (req, res) => {
+    console.log('this is profile..',new Date());
+  res.send('this is profile..'+new Date())
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
